b0720077e615de8edf811d20bf81ab47534e8526
sekrel <56014728+sekrel@users.noreply.github.com>
2024-08-29 05:12:03 +0300

    Update settings.py
    удаление токена джанго

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ywt^i-jce1cq%5q4s^p0130-h9x9vdmvmge-vf8g+htkga+=71'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
